5839b4f5d2b45a1b37000d90732a2b6b1164545a
Evgeniy Ginzburg <nad.oby@gmail.com>
2019-03-07 22:14:47 +0100

    Add allocate function, clang-format

--- mallocator.c
+++ mallocator.c
@@ -1,14 +1,14 @@
+#include "cmdline.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-
-#include "cmdline.h"
+#include <unistd.h>
 
 /*
  * TODO use systen dependant checks http://nadeausoftware.com/articles/2012/09/c_c_tip_how_get_physical_memory_size_system
  * TODO check swap, used memory
  */
+
 unsigned long getTotalSystemMemory()
 {
     long pages = sysconf(_SC_PHYS_PAGES);
@@ -16,7 +16,29 @@ unsigned long getTotalSystemMemory()
     return pages * page_size;
 }
 
-int main(int argc, char ** argv) {
+int allocate()
+{
+    int* ptr;
+    int mb = 1048576;
+    int pg_sz = 4096;
+
+    long long int i;
+
+    for (i = 0; i < 1000000000000; i++) {
+        printf("Trying to allocate %lld MBytes\n", i * pg_sz * sizeof(int) / mb);
+
+        ptr = (int*)calloc(pg_sz, sizeof(int));
+        if (ptr == 0) {
+            // clean up
+            free(ptr);
+            printf("Ran out of memory\n");
+            return 1;
+        }
+    }
+}
+
+int main(int argc, char** argv)
+{
     int MULTIPLICATOR;
     char UNITS[16];
     unsigned long ALLMEM;
@@ -26,27 +48,27 @@ int main(int argc, char ** argv) {
         exit(1);
     }
     /*
-    printf("ai.units_arg: %s\n", ai.units_arg);
-    printf("ai.mem_arg: %d\n", ai.mem_arg);
-    printf("ai.percentage_arg: %d\n", ai.percentage_arg);
-    */
-    switch(*ai.units_arg){
-        case 'K':
-            MULTIPLICATOR = 1024;
-            strcpy(UNITS ,"Kilobytes");
-            break;
-        case 'M':
-            MULTIPLICATOR = 1024*1024;
-            strcpy(UNITS ,"Megabytes");
-            break;
-        case 'G':
-            MULTIPLICATOR = 1024*1024*1024;
-            strcpy(UNITS ,"Gigabytes");
-            break;
+       printf("ai.units_arg: %s\n", ai.units_arg);
+       printf("ai.mem_arg: %d\n", ai.mem_arg);
+       printf("ai.percentage_arg: %d\n", ai.percentage_arg);
+       */
+    switch (*ai.units_arg) {
+    case 'K':
+        MULTIPLICATOR = 1024;
+        strcpy(UNITS, "Kilobytes");
+        break;
+    case 'M':
+        MULTIPLICATOR = 1024 * 1024;
+        strcpy(UNITS, "Megabytes");
+        break;
+    case 'G':
+        MULTIPLICATOR = 1024 * 1024 * 1024;
+        strcpy(UNITS, "Gigabytes");
+        break;
     }
     if (ai.mem_arg != 0) {
     }
-    ALLMEM = getTotalSystemMemory()/MULTIPLICATOR;
+    ALLMEM = getTotalSystemMemory() / MULTIPLICATOR;
     printf("System memory usage is : %zu %s\n", ALLMEM, UNITS);
     exit(0);
 }
